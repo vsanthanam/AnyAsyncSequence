489f9bfe115e6487f463ca206ee15afe3bc888f2
Varun Santhanam <talkto@vsanthanam.com>
2022-05-20 08:53:57 -0700

    Update Site Generator

--- generate-static-site.sh
+++ generate-static-site.sh
@@ -6,7 +6,7 @@ if [[ "$branch" != "main" ]]; then
 else
     git branch -D gh-pages
     git checkout -b gh-pages
-    swift package --allow-writing-to-directory docs generate-documentation --target AnyAsyncSequence --disable-indexing --transform-for-static-hosting --hosting-base-path anyasyncsequence/docs --output-path docs
+    swift package --allow-writing-to-directory docs generate-documentation --target AnyAsyncSequence --disable-indexing --transform-for-static-hosting --hosting-base-path AnyAsyncSequence/docs --output-path docs
     git add .
     git commit -m 'Synchronize Hompage & Publish Documentation'
     git push -f -u origin gh-pages
